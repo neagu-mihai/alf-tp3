5eec0f5e5693a433c9162fbf153f19c79ed5cb02
unduhhh <mihai_neagu_09@yahoo.com>
2022-03-22 17:23:48 +0200

    ex4 pt1

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const antlr4ts_1 = require("antlr4ts");
 const AlfLexer_1 = require("./AlfLexer");
 const AlfParser_1 = require("./AlfParser");
+const fs = __importStar(require("fs"));
 // Create the lexer and parser
 let input = "10\n20";
 let inputStream = antlr4ts_1.CharStreams.fromString(input);
@@ -11,3 +35,16 @@ let tokenStream = new antlr4ts_1.CommonTokenStream(lexer);
 let parser = new AlfParser_1.AlfParser(tokenStream);
 // Parse the input, where `prog` is whatever entry point you defined
 let tree = parser.prog();
+let file_content;
+try {
+    file_content = fs.readFileSync('Alf.tokens', 'ascii');
+    let ln = file_content.split('\n');
+    for (let i in ln) {
+        let ln2 = ln[i].split('=');
+        console.log(ln2);
+    }
+    console.log(file_content);
+}
+catch (error) {
+    console.log(error);
+}
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import {  CharStreams, CodePointCharStream, CommonTokenStream } from 'antlr4ts';
 import { AlfLexer } from './AlfLexer';
 import { AlfParser, ProgContext } from './AlfParser';
+import * as fs from 'fs';
  
 // Create the lexer and parser
 let input: string = "10\n20";
@@ -11,3 +12,16 @@ let parser: AlfParser = new AlfParser(tokenStream);
  
 // Parse the input, where `prog` is whatever entry point you defined
 let tree:ProgContext = parser.prog();
+let file_content:string;
+try {
+    file_content = fs.readFileSync('Alf.tokens', 'ascii');
+    let ln=file_content.split('\r\n');
+    for(let i in ln)
+    {
+        let ln2=ln[i].split('=');
+        console.log(ln2);
+    }
+    console.log(file_content);
+} catch(error) {
+    console.log(error);
+}
