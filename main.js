820372bee336ffeba7cee52927a4c2efb068cdb3
unduhhh <mihai_neagu_09@yahoo.com>
2022-03-26 22:21:21 +0200

    ex1

--- main.js
+++ main.js
@@ -79,3 +79,14 @@ try {
 catch (error) {
     console.log(error);
 }
+let books_array = [
+    'Engineering a Compiler,9780120884780,7th February 2011',
+    'Modern Operating Systems 4,9780133591620,21st March 2014',
+    'Computer Networks,9332518742,9th January 2010'
+];
+let regex = /\,/;
+for (let i in books_array) {
+    let ln = books_array[i].split(regex);
+    let b = { titre: ln[0], isbn: +ln[1], date_de_publication: ln[2] };
+    console.log(b.titre + " " + b.isbn + " " + b.date_de_publication);
+}
--- main.ts
+++ main.ts
@@ -62,3 +62,20 @@ try {
     
 }
 
+let books_array: string[] = [
+    'Engineering a Compiler,9780120884780,7th February 2011',
+    'Modern Operating Systems 4,9780133591620,21st March 2014',
+    'Computer Networks,9332518742,9th January 2010'
+];
+type Book={
+    titre:String;
+    isbn:number;
+    date_de_publication:String;
+}
+let regex:RegExp= /\,/
+for(let i in books_array)
+{
+    let ln=books_array[i].split(regex);
+    let b:Book={titre:ln[0],isbn:+ln[1],date_de_publication:ln[2]}
+    console.log(b.titre+" "+b.isbn+" "+b.date_de_publication);
+}
